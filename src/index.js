60dfb8ab8c05389bd872d65a99c594cf86f26108
Bogdan <bogdantisenko06@gmail.com>
2024-01-19 00:56:34 +0200

    happend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import { program } from "commander";
+const { program } = require("commander");
+const {
+  listContacts,
+  addContact,
+  getContactById,
+  removeContact,
+} = require("./contacts.js");
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,23 +17,26 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const all = await listContacts();
+      console.log(all);
       break;
 
     case "get":
-      // ... id
+      const byId = await getContactById(id);
+      console.log(byId);
       break;
 
     case "add":
-      // ... name email phone
+      const newCon = await addContact(name, email, phone);
+      console.log(newCon);
       break;
 
     case "remove":
-      // ... id
+      const removed = await removeContact(id);
+      console.log(removed);
       break;
 
     default:
